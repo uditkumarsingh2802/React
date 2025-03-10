c4ba85fa4c135740caac3008cf868a225db8cab8
Udit Kumar Singh <uditkumarsingh2802@gmail.com>
2025-03-10 23:52:46 +0530

    Resolved merge conflicts

---
+++ src/components/RTE.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+import { Controller } from 'react-hook-form';
+
+export default function RTE({ name, control, label, defaultValue = "" }) {
+  return (
+    <div className="w-full">
+      {label && <label className="inline-block mb-1 pl-1">{label}</label>}
+
+      <Controller
+        name={name || "content"}
+        control={control}
+        render={({ field: { onChange } }) => (
+          <Editor
+            initialValue={defaultValue}
+            apiKey="q43pfzck1vo53p9rzpcxwus4zxnftyi6xbd6npkr4qui30np"
+            init={{
+               // Use your API key here
+              height: 500,
+              menubar: true,
+              plugins: [
+                "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "help",
+                "wordcount",
+              ],
+              toolbar:
+                "undo redo | blocks | bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+            }} 
+            onEditorChange={onChange}
+          />
+        )}
+      />
+    </div>
+  );
+}
